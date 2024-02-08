07ab5981611da5b50793eb078a57dc3c225909fe
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2024-02-08 11:23:05 +0530

    Create Recursion 4 (Fibonacii Series).java

---
+++ Recursion 4 (Fibonacii Series).java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main
+{
+    public static void fibonacii(int a ,int b, int n)
+    {
+        
+        if(n==0)
+        {
+            return;
+        }
+        int c=(a+b);
+        System.out.println(c);
+        fibonacii(b,c,n-1);
+    }
+    
+	public static void main(String[] args) {
+	    
+	    Scanner scan = new Scanner(System.in);
+		System.out.print("n:");
+		int n= scan.nextInt();
+		
+		int a=0;
+		int b=1;
+		
+		System.out.println("Fibonacii Series: ");
+		System.out.println(a);
+		System.out.println(b);
+		
+		fibonacii(a,b,n-2);
+	}
+}
